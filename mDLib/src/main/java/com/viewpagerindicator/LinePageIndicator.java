70ecec8f2fb0d1fa0a04d018fc7235a360bbf7aa
zengxuanye <zengxuanye@midaigroup.com>
2017-02-24 16:45:26 +0800

    移除volley

--- mDLib/src/main/java/com/viewpagerindicator/LinePageIndicator.java
+++ mDLib/src/main/java/com/viewpagerindicator/LinePageIndicator.java
@@ -373,7 +373,7 @@ public class LinePageIndicator extends View implements PageIndicator {
                 result = Math.min(result, specSize);
             }
         }
-        return (int)FloatMath.ceil(result);
+        return (int)Math.ceil(result);
     }
 
     /**
@@ -399,7 +399,7 @@ public class LinePageIndicator extends View implements PageIndicator {
                 result = Math.min(result, specSize);
             }
         }
-        return (int)FloatMath.ceil(result);
+        return (int)Math.ceil(result);
     }
 
     @Override
